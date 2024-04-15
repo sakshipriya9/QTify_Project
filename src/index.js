cf636db484b6f8ec046fd782932f0c57d70a66e2
Sakshi Priya <priyasakshi8989@gmail.com>
2024-04-15 15:27:09 +0530

    Correction done

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import reportWebVitals from './reportWebVitals';
 
 import './index.css';
 
@@ -15,4 +15,4 @@ root.render(
   </React.StrictMode>
 );
 
-reportWebVitals();
+// reportWebVitals();
