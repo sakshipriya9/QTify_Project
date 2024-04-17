15c7f9b2179dab4ff7fc5da22f741b4ccdb5a3cc
Sakshi Priya <priyasakshi8989@gmail.com>
2024-04-17 11:28:26 +0530

    correction done

--- src/components/Buttons/Buttons.jsx
+++ src/components/Buttons/Buttons.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './Buttons.css';
 
 const Buttons = ({ text, handleOpen }) => (
-  <button className="btn" onClick={handleOpen}>
+  <button className="btn" onClick={handleOpen} style={{ cursor: 'pointer' }}>
     {text}
   </button>
 );
