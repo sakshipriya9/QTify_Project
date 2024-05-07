09793e8c0e7d7c2332e0472fa6738b83d0ec4907
Sakshi Priya <priyasakshi8989@gmail.com>
2024-05-07 21:50:02 +0530

    Added Cards

--- src/App.js
+++ src/App.js
@@ -1,17 +1,61 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react'
 import NavBar from './components/NavBar/NavBar';
-import HeroSection from './components/HeroSection/HeroSection';
-import './components/NavBar/NavBar.css';
+import HerSection from './components/HeroSection/HeroSection';
+import GridSection from './components/GridSection/GridSection';
+import { fetchNewAlbums, fetchTopAlbums, fetchSongs } from './api/api';
 
-function App() {
+const App = () => {
+
+  const [dataTopAlbums, setDataTopAlbums] = useState([]);
+  const [dataNewAlbum, setDataNewAlbum] = useState([]);
+  const [dataSong, setDataSong] = useState([]);
+
+  const getDataTopAlbums = async () => {
+    try {
+      const res = await fetchTopAlbums();
+      setDataTopAlbums(res);
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  const getDataNewAlbums = async () => {
+    try {
+      const res = await fetchNewAlbums();
+      setDataNewAlbum(res);
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  const getDataSongs = async () => {
+    try {
+      const res = await fetchSongs();
+      setDataSong(res);
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  useEffect(() => {
+    getDataTopAlbums();
+    getDataNewAlbums();
+    getDataSongs()
+  }, [])
+
   return (
-    <div className="App">
-      <NavBar />
-      <HeroSection />
-      {/* Other components or content */}
-      {/* Other components or content */}
+    <div>
+      <NavBar data={dataTopAlbums} />
+      <HerSection />
+      <GridSection title={"Top Albums"} data={dataTopAlbums} type={"album"} />
+      <GridSection title={"New Albums "} data={dataNewAlbum} type={"album"} />
+      {/* <Line />
+      <GridSection title={"Songs "} data={dataSong} type={"song"} setDataSong={setDataSong} />
+      <FAQAccordion />
+      <Line />
+      <MusicPlayer data={dataTopAlbums} /> */}
     </div>
-  );
+  )
 }
 
 export default App;
